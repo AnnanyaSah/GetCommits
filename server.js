200483996d06fa70357129120c5cc3436f117a56
AnnanyaSah <annanya.red@gmail.com>
2016-08-27 17:41:37 +0530

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/home', express.static(__dirname + '/home'));
@@ -8,7 +9,7 @@ app.get('/', function (req, res) {
   res.send('Hello from Node Server!');
 });
 
-app.listen(80, function () {
-  console.log('Node Server listening on port 80!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
